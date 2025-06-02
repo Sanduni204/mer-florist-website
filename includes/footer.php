fc0f234deef14a9797bb27ced30e1e232913db2e
Sanduni204 <sandunihansika001@gmail.com>
2025-06-02 21:54:50 +0530

    Add includes

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+  
+    <footer>
+        <div class ="icons">
+            <a href "#"><i class="fa-brands fa-facebook"></i></a>
+            <a href "#"><i class="fa-brands fa-instagram"></i></a>
+            <a href "#"><i class="fa-brands fa-twitter"></i></a>
+            <a href "#"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href "#"><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        
+        <p>copywrite &copy; 2024 mer ; designed by SH Galbadaarachchi ; All Rights Reserved</p>
+    </footer>
+    <script type="text/javascript" src="1javafile.js"></script>
+    </p>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mer | contact</title>
+    <link rel="stylesheet" type="text/css" href="1style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+
+<body>
+    <header>
+    
+    <div class="menubar">
+        <nav>
+            <ul class="main">
+                <li><a href="1home.html" id="home-link" class="navlink">Home</a></li>
+                <li><a href="1catalogue.html" id="catalogue-link" class="navlink">Shop</a></li>
+                <li><a href="1contact.html" id="contact-link" class="navlink">Contact</a></li>
+                <img src=".\Images\logo.png" class="logo">
+
+            </ul>
+        </nav>
+    </div>
+</header>
