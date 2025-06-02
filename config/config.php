d1660eaff6fab315c79c759b031ba413e9288a52
Sanduni204 <sandunihansika001@gmail.com>
2025-06-02 21:50:30 +0530

    Create config file

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+try{
+//host
+define("HOSTNAME", "localhost");
+
+//DBNAME
+define("DBNAME", "mer");
+
+//user
+define("USER", "root");
+
+//pass
+define("PASS", "");
+
+$conn = new PDO("mysql:host=".HOSTNAME.";dbname=".DBNAME.";",USER,PASS);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+catch(PDOException $e)
+{
+    die("Database connection failed: " . $e->getMessage());
+}
