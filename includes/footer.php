ed057bac2c637763663ebbe7ba2c68a610942aac
Sanduni204 <sandunihansika001@gmail.com>
2025-06-06 15:51:40 +0530

    feat(footer): update layout and styling in PHP and CSS

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,15 @@
   
     <footer>
-        <div class ="icons">
-            <a href ="#"><i class="fa-brands fa-facebook"></i></a>
-            <a href ="#"><i class="fa-brands fa-instagram"></i></a>
-            <a href ="#"><i class="fa-brands fa-twitter"></i></a>
-            <a href ="#"><i class="fa-brands fa-whatsapp"></i></a>
-            <a href ="#"><i class="fa-brands fa-youtube"></i></a>
-        </div>
-        
-        <p>copywrite &copy; 2024 mer ; designed by SH Galbadaarachchi ; All Rights Reserved</p>
-    </footer>
+    <div class="icons">
+        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+    </div>
+    
+    <p>Copyright &copy; 2024 mer ; designed by SH Galbadaarachchi ; All Rights Reserved</p>
+</footer>
    
     </p>
     <script type="text/javascript" src="<?php echo APPURL; ?>/1javafile.js?v=<?php echo time(); ?>"></script>
