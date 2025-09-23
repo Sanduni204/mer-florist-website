6c7e1d893d5e5571b632a1a7fdfd9d0a58d9d127
Sanduni204 <sandunihansika001@gmail.com>
2025-09-23 14:28:42 +0530

    Fix:Logout error

--- admin/logout.php
+++ admin/logout.php
@@ -4,6 +4,15 @@ session_start();
 if (!defined('APPURL')) {
 	define('APPURL', 'http://localhost/mer_ecommerce/');
 }
-unset($_SESSION['is_admin'], $_SESSION['admin_email']);
-header('Location: ' . APPURL);
+// Clear all session data (admin + normal user)
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+	$params = session_get_cookie_params();
+	setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
+session_unset();
+session_destroy();
+
+// Send everyone to the public home page after logout
+header('Location: ' . APPURL . '1home.php');
 exit;
